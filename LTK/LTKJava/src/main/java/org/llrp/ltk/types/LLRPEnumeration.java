78c9fbe023361c11886a9b5d8dcd4aef0c573dc6
matthias-lampe <matthias-lampe>
2007-10-19 10:39:08 +0000

    *** empty log message ***

---
+++ LTK/LTKJava/src/main/java/org/llrp/ltk/types/LLRPEnumeration.java
@@ -0,0 +1,41 @@
+package org.llrp.ltk.types;
+
+
+/**
+ * LLRPEnumeration for parameters that allow only
+ * certain values.
+ *
+ */
+public interface LLRPEnumeration {
+    /**
+     * check if a value is allowed for an enumeration.
+    * @param name
+     * @return boolean
+    */
+    public boolean isValidValue(final Integer value);
+
+    /**
+     * get value of a name representing a value
+     * if no value can be found, return -1.
+     * @param name
+     * @return int
+     */
+    public int getValue(final String name);
+
+    /**
+     * get name of a value
+     * if no name can be found for given value
+     * return empty string.
+     * @param name
+     * @return String
+     */
+    public String getName(int value);
+
+    /**
+             * check if the name stands for an allowed value
+            * of this enumeration.
+            * @param name
+             * @return boolean
+     */
+    public boolean isValidName(final String name);
+}
