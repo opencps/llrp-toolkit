9ff85d52f13c278e77f4cf088149935909915497
jhoger <jhoger>
2008-07-08 18:11:13 +0000

    Initial xml2llrp build check-in: stub implementation only.

---
+++ LTK/LTKNet/XML2LLRP/XML2LLRP.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using LLRP;
+using LLRP.DataType;
+
+namespace LTK
+{
+    class XML2LLRP
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NOT IMPLEMENTED");
+        }
+    }
+}
