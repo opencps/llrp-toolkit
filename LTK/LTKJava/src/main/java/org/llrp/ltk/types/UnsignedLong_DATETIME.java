4eb93047eaf7c866569fe1ab7c26bcb666dfdac4
floerkem <floerkem>
2008-07-09 03:35:15 +0000

    encodeXML and decodeXML previously did not encode microseconds correctly.

--- LTK/LTKJava/src/main/java/org/llrp/ltk/types/UnsignedLong_DATETIME.java
+++ LTK/LTKJava/src/main/java/org/llrp/ltk/types/UnsignedLong_DATETIME.java
@@ -1,11 +1,15 @@
 package org.llrp.ltk.types;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
-import javax.xml.bind.DatatypeConverter;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 
 import org.jdom.Content;
 import org.jdom.Element;
@@ -70,28 +74,71 @@ public class UnsignedLong_DATETIME extends UnsignedLong {
 
 	@Override
 	public void decodeXML(Element element) {
-		java.util.Calendar cal = DatatypeConverter.parseDateTime(element.getText());
-		value = BigInteger.valueOf(cal.getTimeInMillis());
+		//java.util.Calendar cal = DatatypeConverter.parseDateTime(element.getText());
+		//value = BigInteger.valueOf(cal.getTimeInMillis());
+		DatatypeFactory df;
+		try {
+			df = DatatypeFactory.newInstance();
+			XMLGregorianCalendar cal = df.newXMLGregorianCalendar(element.getText());
+			
+			// convert to BigInteger format with value in microseconds 
+			// the last three digits ("microseconds") are "000" at this stage
+			value = BigInteger.valueOf(cal.toGregorianCalendar().getTimeInMillis()*1000);
+			
+			// compute microseconds value by subtracting milliseconds from return value
+			// of XMLGregorianCalendar.getFractionalSecond()
+			BigDecimal millisec = cal.getFractionalSecond().setScale(3, BigDecimal.ROUND_DOWN);
+			BigDecimal microsec = (cal.getFractionalSecond().setScale(6,BigDecimal.ROUND_DOWN)).subtract(millisec);;
+			
+			// add microseconds 
+			value = value.add(microsec.movePointRight(6).toBigInteger());
+			
+		} catch (DatatypeConfigurationException e) {
+			throw new IllegalArgumentException(e.getMessage());
+		}
+		
 	}
 
 	@Override
 	public Content encodeXML(String name, Namespace ns) {
-		SimpleDateFormat dateFormat = new SimpleDateFormat(
-				"yyyy-MM-dd'T'hh:mm:ss.SZ");
-		Date date = new Date(value.longValue());
+		
+		GregorianCalendar cal = new GregorianCalendar();
+		
+		// initialize calendar after removing the last 
+		// three digits that represent microseconds 
+		long milliseconds = value.longValue()/1000;
+		cal.setTimeInMillis(milliseconds);
+		
+		StringBuffer sb = new StringBuffer();
+		DatatypeFactory df;
+		try {
+			df = DatatypeFactory.newInstance();
+			XMLGregorianCalendar xmlcal = df.newXMLGregorianCalendar(cal);
+		    sb.append(xmlcal.toXMLFormat());
+		    
+		} catch (DatatypeConfigurationException e) {
+			throw new IllegalArgumentException(e.getMessage());
+		}
+		
+		int indexOfT = sb.indexOf("T");
+		int l = value.toString().length();
+		String microseconds = value.toString().substring(l-3, l);
+		sb.insert(indexOfT+13, microseconds);
+		
 		Element element = new Element(name, ns);
 		
 		// to convert to xsd:dateTime it is necessary to insert a ":" 
 		// in the timezone generated using the "Z" in the 
 		// SimpleDateFormat expression above
-		StringBuffer sb = new StringBuffer();
-		sb.append(dateFormat.format(date));
-		sb.insert(sb.length()-2, ":");
+	
+		//sb.append(dateFormat.format(date));
+		//sb.insert(sb.length()-2, ":");
 		
 		element.setContent(new Text(sb.toString()));
 
 		return element;
 	}
+	
 	
 	public String toString(){
 		return encodeXML("foo", Namespace.getNamespace("foo")).getValue();
