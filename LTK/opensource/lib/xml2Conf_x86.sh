b04c8c3306dfcb661ca91430867ce3ab9f60cb06
pauldietrich <pauldietrich>
2010-02-23 15:27:46 +0000

    add the libxml2 headers and libraries

---
+++ LTK/opensource/lib/xml2Conf_x86.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/pdietric/Development/LLRP-Toolkit/ImpinjInternal/ltk/opensource/libxml2-2.7.4/../_x86/lib"
+XML2_LIBS="-lxml2    -lm "
+XML2_INCLUDEDIR="-I/home/pdietric/Development/LLRP-Toolkit/ImpinjInternal/ltk/opensource/libxml2-2.7.4/../include/libxml2"
+MODULE_VERSION="xml2-2.7.4"
+
